b77db541cfbbb13a76602996432850cb00878d60
PreetPadaliya <preetpadaliya2805@gmail.com>
2025-07-16 19:07:34 +0530

    Patient Section Completed

--- src/Management/Patient.java
+++ src/Management/Patient.java
@@ -2,6 +2,7 @@ package Management;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
 public class Patient{
@@ -37,8 +38,47 @@ public class Patient{
 
         } catch (Exception e) {
             e.printStackTrace();
-
         }
+    }
+    public void viewPatient(){
+        String query = "select * from patients";
 
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            System.out.println("Patients: ");
+            System.out.println("+------------+---------------+-------+--------+");
+            System.out.println("| Patient Id | Name          |  Age  | Gender |");
+            System.out.println("+------------+---------------+-------+--------+");
+            while (resultSet.next()){
+                int id  = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                String gender = resultSet.getString("gender");
+                System.out.println("+------------+---------------+-------+--------+");
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean getPatientById(int id) {
+        String query = "select * from patients where id = ?";
+        try{
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1,id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()){
+            return true;
+        }
+        else {
+            return false;
+        }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
